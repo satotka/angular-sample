b6523a3019ad9f0a9073a7a818805cb1a2fd56b9
satotka <takaaki.satoh@gmail.com>
2019-07-06 14:57:10 +0000

    refactor: using item class

--- mytest/src/app/items/items.component.ts
+++ mytest/src/app/items/items.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit, Input } from '@angular/core';
+import { Item } from '../item';
 
 @Component({
   selector: 'app-items',
@@ -7,7 +8,7 @@ import { Component, OnInit, Input } from '@angular/core';
 })
 export class ItemsComponent implements OnInit {
 
-  @Input() items;
+  @Input() items: Item[];
 
   constructor() { }
 
