c338884aac9a1c40da717b7742465ce3eb3db7c6
satotka <takaaki.satoh@gmail.com>
2019-07-06 16:43:36 +0000

    test: more test data.

--- mytest/src/app/item.service.ts
+++ mytest/src/app/item.service.ts
@@ -11,7 +11,10 @@ export class ItemService {
   getItems(): Item[] {
     return [
       { id: 1, name: 'item1', complete: false, type: '装置' },
-      { id: 2, name: 'item2', complete: false, type: 'エリア' }
+      { id: 2, name: 'item2', complete: false, type: '装置' },
+      { id: 3, name: 'item3', complete: false, type: 'エリア' },
+      { id: 4, name: 'item4', complete: false, type: 'エリア' },
+      { id: 5, name: 'item5', complete: false, type: 'エリア' }
     ];
   }
 }
